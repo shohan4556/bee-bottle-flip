f46c7473fc9098c1d949c1ec22802bdff98c4e5f
shohan <shohan4556@gmail.com>
2020-06-20 16:16:29 +0600

    updated

---
+++ Assets/_Scripts/Bucket.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bucket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionStay2D(Collision2D other) {
+        if (other.gameObject.tag.Equals("Player")) {
+            other.transform.parent = this.transform;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other) {
+        if (other.gameObject.tag.Equals("Player")) {
+            other.transform.parent = null;
+        }
+    }
+    
+}
---
+++ Assets/_Scripts/Infinity Level/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace GM_Infinity
+{
+    public class PlayerController : MonoBehaviour
+    {
+        private Rigidbody2D _rigidbody;
+        public float jumpForce = 10f;
+        [SerializeField]
+        private bool _isGrounded = true;
+
+        [SerializeField] 
+        public float nextTapTime = 0.10f;
+        
+        // Start is called before the first frame update
+        void Start() {
+            _rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        void FixedUpdate() {
+            if (Input.GetMouseButtonDown(0) && _isGrounded && nextTapTime <= 0f) {
+                print("tapped");
+                //_rigidbody.velocity += Vector2.up * jumpForce;
+                _rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                nextTapTime -= Time.deltaTime;
+            }
+
+            if (Physics2D.Raycast(transform.position, Vector2.down, 0.35f)) {
+                _isGrounded = true;
+            }
+            else {
+                _isGrounded = false;
+                nextTapTime = 0.2f;
+            }
+            
+            Debug.DrawRay(transform.position, Vector2.down * 0.35f, Color.red);
+            
+        } // end 
+
+    }
+}
