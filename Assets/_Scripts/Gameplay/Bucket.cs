7e19646c0829e595c84c00b1b04c451f09d4f287
shohan <shohan4556@gmail.com>
2020-06-21 17:32:09 +0600

    bucket movement pinpong

--- Assets/_Scripts/Gameplay/Bucket.cs
+++ Assets/_Scripts/Gameplay/Bucket.cs
@@ -10,45 +10,50 @@ public class Bucket : MonoBehaviour
     [SerializeField] private Transform coinSpawnPos;
 
     [SerializeField] private bool isSpawnCoin = false;
+    [SerializeField] private bool isMove = false;
     
-    [SerializeField] public AnimationCurve _curve;
     private Vector2 pos1;
     private Vector2 pos2;
+
+    private float moveSpeed;
+    private float rndLength;
     
     // Start is called before the first frame update
     void Start() {
 
-        pos1 = transform.position;
-        pos2 = new Vector2(pos1.x+1f, pos1.y);
-        //StartCoroutine(Move(pos1, pos2, _curve, 4f));
+        RandomMovememt();
+
+        pos1 = new Vector2(transform.position.x-1f, transform.position.y);
+        pos2 = new Vector2(transform.position.x+1f, transform.position.y);
+        
+        float rand = Random.Range(1f, 100f);
+        // 50% chance to move or stay freez
+        if (rand >= 50) {
+            isMove = true;
+        }
         
         if (!isSpawnCoin) {
             return;
         }
         
-        float rand = Random.Range(1f, 100f);
         // 50% chance to spawn coin
         if (rand >= 50) {
             Instantiate(coinPrefab, coinSpawnPos.position, Quaternion.identity, transform);
         }
     }
 
-    IEnumerator Move ( Vector3 pos1, Vector3 pos2, AnimationCurve ac, float time ) {
-        float timer = 0.0f;
-        while (timer <= time) {
-            transform.position = Vector3.Lerp (pos1, pos2, ac.Evaluate (timer / time));
-            timer += Time.deltaTime;
-            
-            print(Vector2.Distance(transform.position, pos2));
-           
-            yield return null;
-        }
-
-        StartCoroutine(Move(pos2, pos1, _curve, 4f));
+    private void RandomMovememt() {
+        moveSpeed = Random.Range(0.25f, 1f);
+        rndLength = Random.Range(0.75f, 1.5f);
     }
 
+
+    private float timer = 0f;
     private void Update() {
-        transform.position = Vector3.Lerp (pos1, pos2, _curve.Evaluate (Time.deltaTime / 4f));
+        if (isMove) {
+            transform.position = Vector3.Lerp(pos1, pos2, Mathf.PingPong(Time.time *moveSpeed, rndLength));
+            timer += Time.deltaTime;
+        }
     }
 
     private void OnCollisionStay2D(Collision2D other) {
